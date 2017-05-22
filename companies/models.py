7294a0e1eccf2022c8ad17d7a9765ede33f39855
Kanha Goyal <goyalkanha11@gmail.com>
2017-05-22 23:37:06 +0530

    models ceated

---
+++ companies/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+
+class Stock(models.Model):
+    ticker = models.CharField(max_length = 10)
+    openvalue = models.FloatField()
+    closevalue = models.FloatField()
+    volume = models.IntegerField()
+
+    def __str__(self):
+        return self.ticker
